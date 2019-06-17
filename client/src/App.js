ac6639553161f5e6f67546db23cf8b4a48220e13
github username <github email address>
2019-06-17 22:59:56 +0100

    Navbar Done

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import AppNavbar from './components/AppNavbar';
+
+function App() {
+  return (
+    <div className="App">
+    <AppNavbar/>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
