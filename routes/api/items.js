718ce66e67fdb6b8819c1321732e77b05421a0b9
github username <github email address>
2019-06-17 20:09:38 +0100

    backend complete

--- routes/api/items.js
+++ routes/api/items.js
@@ -19,5 +19,14 @@ router.post('/', (req, res) => {
 
 })
 
+//Route DELETE /api/items
+
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true})))
+    .catch(err => res.status(404).json({success: false}))
+})
+
+
 
 module.exports = router
